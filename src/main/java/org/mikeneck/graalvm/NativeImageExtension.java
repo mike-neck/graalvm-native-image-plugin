27b422ccbfd11812a82b417847846ea78d4eb3d9
mike_neck <jkrt3333@gmail.com>
2019-11-09 22:40:17 +0900

    initial commit

---
+++ src/main/java/org/mikeneck/graalvm/NativeImageExtension.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2019 Shinya Mochida
+ *
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mikeneck.graalvm;
+
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.internal.provider.DefaultProvider;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.ListProperty;
+import org.gradle.api.provider.Property;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class NativeImageExtension {
+
+    final Property<String> graalVmHome;
+    final Property<Task> jarTask;
+    final Property<String> mainClass;
+    final Property<String> executableName;
+    final Property<Configuration> runtimeClasspath;
+    final ListProperty<String> additionalArguments;
+
+    NativeImageExtension(Project project) {
+        ObjectFactory objects = project.getObjects();
+        this.graalVmHome = objects.property(String.class);
+        this.jarTask = objects.property(Task.class);
+        this.mainClass = objects.property(String.class);
+        this.executableName = objects.property(String.class);
+        this.runtimeClasspath = objects.property(Configuration.class);
+        this.additionalArguments = objects.listProperty(String.class);
+
+        this.graalVmHome.set((System.getProperty("java.home")));
+        configureDefaultJarTask(project);
+        configureDefaultRuntimeClasspath(project);
+    }
+
+    private void configureDefaultJarTask(Project project) {
+        this.jarTask.set(new DefaultProvider<>(() -> project.getTasks().getByName("jar")));
+    }
+
+    private void configureDefaultRuntimeClasspath(Project project) {
+        this.runtimeClasspath.set(new DefaultProvider<>(() -> project.getConfigurations().getByName("runtimeClasspath")));
+    }
+
+    public void setGraalVmHome(String graalVmHome) {
+        this.graalVmHome.set(graalVmHome);
+    }
+
+    public void setJarTask(Task jarTask) {
+        this.jarTask.set(jarTask);
+    }
+
+    public void setMainClass(String mainClass) {
+        this.mainClass.set(mainClass);
+    }
+
+    public void setExecutableName(String name) {
+        this.executableName.set(name);
+    }
+
+    public void setRuntimeClasspath(Configuration configuration) {
+        this.runtimeClasspath.set(configuration);
+    }
+
+    public void arguments(String... arguments) {
+        List<String> list = Arrays.stream(arguments)
+                .filter(Objects::nonNull)
+                .filter(it -> !it.isEmpty())
+                .collect(Collectors.toList());
+        this.additionalArguments.addAll(list);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("GraalExtension{");
+        sb.append("graalVmHome=").append(graalVmHome);
+        sb.append(", jarTask=").append(jarTask);
+        sb.append(", mainClass=").append(mainClass);
+        sb.append(", executableName=").append(executableName);
+        sb.append(", runtimeClasspath=").append(runtimeClasspath);
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ src/main/java/org/mikeneck/graalvm/NativeImageTask.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2019 Shinya Mochida
+ *
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mikeneck.graalvm;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.InvalidUserDataException;
+import org.gradle.api.Project;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.OutputFile;
+import org.gradle.api.tasks.TaskAction;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NativeImageTask extends DefaultTask {
+
+    private Property<NativeImageExtension> extension;
+
+    public NativeImageTask() {
+        this.extension = getProject().getObjects().property(NativeImageExtension.class);
+    }
+
+    public void setExtension(NativeImageExtension extension) {
+        this.extension.set(extension);
+    }
+
+    @TaskAction
+    public void createNativeImage() {
+        createOutputDirectoryIfNotExisting();
+        Path nativeImageCommand = nativeImageCommand();
+        getProject().exec(execSpec -> {
+            getLogger().info("run native-image binary.");
+            execSpec.setExecutable(nativeImageCommand);
+            execSpec.args(arguments());
+        });
+    }
+
+    private Path nativeImageCommand() {
+        Path graalVmHome = graalVmHome();
+        Path nativeImageCommand = graalVmHome.resolve("bin/native-image");
+        if (!Files.exists(nativeImageCommand)) {
+            getLogger().warn("native-image not found in graalVmHome({})", graalVmHome);
+            throw new InvalidUserDataException("native-image not found in graalVmHome(" + graalVmHome + ")");
+        }
+        return nativeImageCommand;
+    }
+
+    private Path graalVmHome() {
+        return Paths.get(extension.get().graalVmHome.get());
+    }
+
+    private File outputDirectory() {
+        Project project = getProject();
+        return project.getBuildDir().toPath().resolve("native-image").toFile();
+    }
+
+    private void createOutputDirectoryIfNotExisting() {
+        File outputDir = outputDirectory();
+        getLogger().info("create output directory if not exists: {}", outputDir);
+        if (!outputDir.exists()) {
+            outputDir.mkdirs();
+        }
+    }
+
+    private Collection<File> runtimeClasspath() {
+        return extension.get().runtimeClasspath.get().getFiles();
+    }
+
+    private File jarFile() {
+        return extension.get().jarTask.get().getOutputs().getFiles().getSingleFile();
+    }
+
+    private List<String> arguments() {
+        List<String> args = new ArrayList<>();
+        args.add("-cp");
+        args.add(classpath());
+        args.add("-H:Path=" + outputDirectory().getAbsolutePath());
+        if (extension.get().executableName.isPresent()) {
+            args.add("-H:Name=" + extension.get().executableName.get());
+        }
+        if (extension.get().additionalArguments.isPresent()) {
+            args.addAll(extension.get().additionalArguments.get());
+        }
+        args.add(extension.get().mainClass.get());
+        return Collections.unmodifiableList(args);
+    }
+
+    private String classpath() {
+        List<File> paths = new ArrayList<>(runtimeClasspath());
+        paths.add(jarFile());
+        return paths.stream()
+                .map(File::getAbsolutePath)
+                .collect(Collectors.joining(":"));
+    }
+
+    @OutputFile
+    public File getOutputExecutable() {
+        return outputDirectory().toPath().resolve(extension.get().executableName.get()).toFile();
+    }
+}
