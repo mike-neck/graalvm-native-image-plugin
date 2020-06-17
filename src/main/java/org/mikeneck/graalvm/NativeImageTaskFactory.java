ee4ab013dd985c4caa95cc49cfac3c5c66079005
mike_neck <jkrt3333@gmail.com>
2020-06-18 01:25:18 +0900

    graalvm-native-image-plugin-34 change TaskFactory.java to NativeImageTaskFactory.java

---
+++ src/main/java/org/mikeneck/graalvm/NativeImageTaskFactory.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2020 Shinya Mochida
+ *
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mikeneck.graalvm;
+
+import java.nio.file.Paths;
+import org.gradle.api.Action;
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.file.ConfigurableFileCollection;
+import org.gradle.api.file.Directory;
+import org.gradle.api.file.ProjectLayout;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.Property;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.provider.ProviderFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class NativeImageTaskFactory {
+
+    @NotNull
+    private final Project project;
+    @NotNull
+    private final Property<GraalVmHome> graalVmHome;
+    @NotNull
+    private final Property<Configuration> runtimeClasspath;
+    @NotNull
+    private final ConfigurableFileCollection jarFile;
+
+    @SuppressWarnings("UnstableApiUsage")
+    NativeImageTaskFactory(@NotNull Project project) {
+        ObjectFactory objectFactory = project.getObjects();
+        ProviderFactory providerFactory = project.getProviders();
+
+        this.project = project;
+        this.graalVmHome = objectFactory
+                .property(GraalVmHome.class)
+                .convention(
+                        providerFactory
+                                .environmentVariable("JAVA_HOME")
+                                .map(Paths::get)
+                                .map(GraalVmHome::new));
+        this.runtimeClasspath = objectFactory
+                .property(Configuration.class)
+                .convention(project.provider(
+                        () -> project
+                                .getConfigurations()
+                                .getByName("runtimeClasspath")));
+        this.jarFile = objectFactory.fileCollection();
+        this.jarFile.builtBy("jar");
+        this.jarFile.setFrom(project.provider(() -> project
+                .getTasks()
+                .getByName("jar")
+                .getOutputs()
+                .getFiles()));
+    }
+
+    NativeImageTask nativeImageTask(@NotNull Action<NativeImageTask> config) {
+        NativeImageTask nativeImage = project.getTasks()
+                .create(
+                        "nativeImage",
+                        DefaultNativeImageTask.class,
+                        graalVmHome,
+                        runtimeClasspath,
+                        jarFile);
+        ProjectLayout projectLayout = project.getLayout();
+        Provider<Directory> outputDirectory = 
+                projectLayout.getBuildDirectory()
+                        .dir(DefaultNativeImageTask.DEFAULT_OUTPUT_DIRECTORY_NAME);
+        nativeImage.setOutputDirectory(outputDirectory);
+        config.execute(nativeImage);
+        return nativeImage;
+    }
+
+    InstallNativeImageTask installNativeImageTask(@NotNull Action<InstallNativeImageTask> config) {
+        InstallNativeImageTask installNativeImage = project.getTasks()
+                .create(
+                        "installNativeImage",
+                        DefaultInstallNativeImageTask.class,
+                        graalVmHome);
+        config.execute(installNativeImage);
+        return installNativeImage;
+    }
+
+    GenerateNativeImageConfigTask generateNativeImageConfigTask(@NotNull Action<GenerateNativeImageConfigTask> config) {
+        GenerateNativeImageConfigTask generateNativeImageConfigTask = project.getTasks()
+                .create(
+                        "generateNativeImageConfigTask",
+                        DefaultGenerateNativeImageConfigTask.class,
+                        project,
+                        graalVmHome,
+                        runtimeClasspath,
+                        jarFile);
+        config.execute(generateNativeImageConfigTask);
+        return generateNativeImageConfigTask;
+    }
+
+    MergeNativeImageConfigTask mergeNativeImageConfigTask(@NotNull Action<MergeNativeImageConfigTask> config) {
+        DefaultMergeNativeImageConfigTask mergeNativeImageConfigTask = project.getTasks()
+                .create(
+                        "mergeNativeImageConfigTask",
+                        DefaultMergeNativeImageConfigTask.class,
+                        project);
+        config.execute(mergeNativeImageConfigTask);
+        return mergeNativeImageConfigTask;
+    }
+}
