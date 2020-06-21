dbd6e269e60af9cb48d0ebfe15d254be69f05c91
mike_neck <jkrt3333@gmail.com>
2020-06-21 17:33:25 +0900

    graalvm-native-image-plugin-34 add check for merge/generate-config tasks to be disabled.

--- src/test/java/org/mikeneck/graalvm/GraalvmNativeImagePluginTest.java
+++ src/test/java/org/mikeneck/graalvm/GraalvmNativeImagePluginTest.java
@@ -5,9 +5,11 @@ package org.mikeneck.graalvm;
 
 import org.gradle.api.Project;
 import org.gradle.api.Task;
+import org.gradle.api.tasks.TaskCollection;
 import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
@@ -26,5 +28,26 @@ public class GraalvmNativeImagePluginTest {
         Task nativeImageTask = project.getTasks().getByName("nativeImage");
         assertNotNull(nativeImageTask);
         assertThat(nativeImageTask, instanceOf(DefaultNativeImageTask.class));
+    }
+
+    @Test
+    public void generateNativeImageConfigTasksAreDisabled() {
+        Project project = ProjectBuilder.builder().build();
+        project.getPlugins().apply("java");
+        project.getPlugins().apply("org.mikeneck.graalvm-native-image");
+
+        TaskCollection<GenerateNativeImageConfigTask> generateNativeImageConfigTasks =
+                project.getTasks().withType(GenerateNativeImageConfigTask.class);
+        assertThat(generateNativeImageConfigTasks)
+                .describedAs("generateNativeTasks")
+                .hasSize(1)
+                .allSatisfy(task -> assertThat(task.getEnabled()).isFalse());
+
+        TaskCollection<MergeNativeImageConfigTask> mergeNativeImageConfigTasks =
+                project.getTasks().withType(MergeNativeImageConfigTask.class);
+        assertThat(mergeNativeImageConfigTasks)
+                .describedAs("mergeNativeImageConfigTasks")
+                .hasSize(1)
+                .allSatisfy(task -> assertThat(task.getEnabled()).isFalse());
     }
 }
