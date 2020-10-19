86f434396ccfd9c780cc318b909c80d77faa250b
mike_neck <jkrt3333@gmail.com>
2020-10-20 08:31:35 +0900

    graalvm-native-image-plugin-87 Fix #87 Auto update versions failed to run

--- script/generate-versions.sh
+++ script/generate-versions.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+set -eu
+
+readonly yj="${YJ}"
+readonly token="${TOKEN}"
+
 curl -s\
     "https://api.github.com/repos/gradle/gradle/releases?per_page=40" \
     -H 'accept: application/vnd.github.v3+json' \
-    -H "authorization: token ${TOKEN}" |
+    -H "authorization: token ${token}" |
 ./script/versions.jq |
-yj -jy > .github/workflows/versions.yml
-
-cat .github/workflows/versions.yml
+"${yj}" -jy |
+tee .github/workflows/versions.yml
