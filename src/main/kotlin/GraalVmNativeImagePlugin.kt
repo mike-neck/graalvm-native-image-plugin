4aa4b7134fc0e8600020d2b8b7dc7b8672802975
mike_neck <jkrt3333@gmail.com>
2020-12-19 18:29:20 +0900

    graalvm-native-image-plugin-101 remove unused import.

--- src/main/kotlin/GraalVmNativeImagePlugin.kt
+++ src/main/kotlin/GraalVmNativeImagePlugin.kt
@@ -5,7 +5,6 @@ import org.gradle.api.tasks.bundling.Jar
 import org.mikeneck.graalvm.GenerateNativeImageConfigTask
 import org.mikeneck.graalvm.GenerateNativeImageConfigTaskWrapper
 import org.mikeneck.graalvm.NativeImageTask
-import org.mikeneck.graalvm.nativeimage.options.Options
 import java.io.File
 
 fun Project.nativeImage(configuration: NativeImageTask.() -> Unit): Unit =
