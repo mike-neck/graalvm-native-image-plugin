7624c46c5a6be372d5fb0cd02641a67155824c69
mike_neck <jkrt3333@gmail.com>
2020-12-19 12:11:45 +0900

    graalvm-native-image-plugin-102 remove license area for spotless check

--- src/main/kotlin/GraalVmNativeImagePlugin.kt
+++ src/main/kotlin/GraalVmNativeImagePlugin.kt
@@ -7,22 +7,6 @@ import org.mikeneck.graalvm.GenerateNativeImageConfigTaskWrapper
 import org.mikeneck.graalvm.NativeImageTask
 import java.io.File
 
-/*
- * Copyright 2020 Shinya Mochida
- * 
- * Licensed under the Apache License,Version2.0(the"License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing,software
- * Distributed under the License is distributed on an"AS IS"BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 fun Project.nativeImage(configuration: NativeImageTask.() -> Unit): Unit =
     when(val nativeImageTask = this.tasks.withType(NativeImageTask::class.java).findByName("nativeImage")) {
       null -> Unit
